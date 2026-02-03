7f8211e4fd34d1d32a586bbb4883a191dbab6e3b
OYJ <oyj@mediazen.co.kr>
2026-02-03 14:38:13 +0900

    Refactor : 가로 바 애니매이션 추가

--- SimpleChart/app/src/main/java/com/example/simplechart/Ex1_5.kt
+++ SimpleChart/app/src/main/java/com/example/simplechart/Ex1_5.kt
@@ -1,17 +1,25 @@
 package com.example.simplechart
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -20,9 +28,10 @@ import androidx.compose.ui.unit.dp
 import com.example.simplechart.ui.theme.SimpleChartTheme
 
 @Composable
-fun Ex1_4() {
+fun Ex1_5() {
 
     val barDataList = listOf(0.2f, 0.4f, 0.6f, 0.8f, 1.0f)
+    val fullWidth = 320.dp
 
     Box(modifier = Modifier.fillMaxSize()) {
 
@@ -34,10 +43,22 @@ fun Ex1_4() {
         ) {
 
             barDataList.forEach {
+
+                var resultWidth by remember { mutableStateOf(0.dp) }
+
+                val animateWidth by animateDpAsState(
+                    targetValue = resultWidth,
+                    animationSpec = tween(durationMillis = 3000, easing = FastOutSlowInEasing)
+                )
+
+                LaunchedEffect(true) {
+                    resultWidth = fullWidth * it
+                }
+
                 Row() {
                     Box(
                         modifier = Modifier
-                            .fillMaxWidth(fraction = (it * 0.8).toFloat())
+                            .width(animateWidth)
                             .height(30.dp)
                             .background(Color.Black, shape = RoundedCornerShape(topEnd = 15.dp, bottomEnd = 15.dp))
                     ) {
@@ -59,8 +80,8 @@ fun Ex1_4() {
 
 @Preview(showBackground = true)
 @Composable
-private fun Ex1_4_Preview() {
+private fun Ex1_5_Preview() {
     SimpleChartTheme {
-        Ex1_4()
+        Ex1_5()
     }
 }
--- SimpleChart/app/src/main/java/com/example/simplechart/MainActivity.kt
+++ SimpleChart/app/src/main/java/com/example/simplechart/MainActivity.kt
@@ -3,40 +3,15 @@ package com.example.simplechart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.simplechart.ui.theme.SimpleChartTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SimpleChartTheme {
-                Ex1_3()
+                Ex1_5()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpleChartTheme {
-        Greeting("Android")
     }
 }
