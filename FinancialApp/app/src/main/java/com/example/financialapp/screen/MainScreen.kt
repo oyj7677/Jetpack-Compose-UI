55bfba5d65d0db7a3b149c3b6f7241b1dac3e946
OYJ <oyj@mediazen.co.kr>
2026-02-04 16:40:12 +0900

    19. 금융앱 소비내역 만들기 -3(TopMenu)

--- FinancialApp/app/src/main/java/com/example/financialapp/screen/MainScreen.kt
+++ FinancialApp/app/src/main/java/com/example/financialapp/screen/MainScreen.kt
@@ -2,17 +2,23 @@ package com.example.financialapp.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.financialapp.ui.theme.FinancialAppTheme
 import com.guru.fontawesomecomposelib.FaIcon
 import com.guru.fontawesomecomposelib.FaIcons
@@ -27,6 +33,8 @@ fun MainScreen(modifier: Modifier = Modifier) {
             .background(Color.Black)
     ) {
         Header()
+        TopMenu()
+        TopMenuBottom()
     }
 }
 
@@ -47,6 +55,82 @@ fun Header() {
             tint = Color.White,
             modifier = Modifier.padding(20.dp)
         )
+    }
+}
+
+@Composable
+fun TopMenu() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp)
+    ) {
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "자산",
+                color = Color.Gray,
+                fontSize = 16.sp
+            )
+        }
+
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "소비·수입",
+                color = Color.White,
+                fontSize = 16.sp
+            )
+        }
+
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "연말 정산",
+                color = Color.Gray,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun TopMenuBottom() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp)
+    ) {
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 15.dp, start = 5.dp, end = 5.dp)
+                .height(2.dp)
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+
+        }
+
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+
+        }
     }
 }
 
