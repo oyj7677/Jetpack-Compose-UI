29e2e65ba61d24c880daf95a4be0cdd799d2fe0c
OYJ <oyj@mediazen.co.kr>
2026-02-04 16:33:01 +0900

    18. 금융앱 소비내역 만들기 -2(Header)

--- FinancialApp/app/src/main/java/com/example/financialapp/MainActivity.kt
+++ FinancialApp/app/src/main/java/com/example/financialapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.financialapp.screen.MainScreen
 import com.example.financialapp.ui.theme.FinancialAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FinancialAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- FinancialApp/app/src/main/java/com/example/financialapp/screen/MainScreen.kt
+++ FinancialApp/app/src/main/java/com/example/financialapp/screen/MainScreen.kt
@@ -1,6 +1,15 @@
 package com.example.financialapp.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -9,13 +18,36 @@ import com.guru.fontawesomecomposelib.FaIcon
 import com.guru.fontawesomecomposelib.FaIcons
 
 @Composable
-fun MainScreen() {
+fun MainScreen(modifier: Modifier = Modifier) {
 
-    FaIcon(
-        faIcon = FaIcons.ChartBarRegular,
-        size = 50.dp,
-        tint = Color.Red
-    )
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(Color.Black)
+    ) {
+        Header()
+    }
+}
+
+@Composable
+fun Header() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        FaIcon(
+            faIcon = FaIcons.ArrowLeft,
+            tint = Color.White,
+            modifier = Modifier.padding(20.dp)
+
+        )
+        FaIcon(
+            faIcon = FaIcons.Plus,
+            tint = Color.White,
+            modifier = Modifier.padding(20.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
