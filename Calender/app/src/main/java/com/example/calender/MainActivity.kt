47fc6e1499aaa7aec4d8537f62646b49737d50aa
OYJ <oyj@mediazen.co.kr>
2026-02-12 15:51:57 +0900

    34. 달력 앱 만들기 - 2 (달력 요일 만들기)

--- Calender/app/src/main/java/com/example/calender/MainActivity.kt
+++ Calender/app/src/main/java/com/example/calender/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,10 +14,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calender.ui.theme.CalenderTheme
 import com.guru.fontawesomecomposelib.FaIcon
 import com.guru.fontawesomecomposelib.FaIcons
@@ -31,44 +32,70 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    CalenderHeader(
+                    Column(
                         modifier = Modifier
                             .padding(innerPadding)
-                    )
+                    ) {
+                        CalenderHeader()
+                        CalenderDayNames()
+                    }
                 }
 
+            }
         }
     }
 }
 
-    @Composable
-    fun CalenderHeader(modifier: Modifier = Modifier) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(top  = 20.dp, bottom = 20.dp),
-            horizontalArrangement = Arrangement.End
-        ) {
-            FaIcon(
-                faIcon = FaIcons.Cog,
-                size = 30.dp,
-                tint = Color.Gray,
-                modifier = Modifier.padding(end = 10.dp)
-            )
+@Composable
+fun CalenderHeader(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp, bottom = 20.dp),
+        horizontalArrangement = Arrangement.End
+    ) {
+        FaIcon(
+            faIcon = FaIcons.Cog,
+            size = 30.dp,
+            tint = Color.Gray,
+            modifier = Modifier.padding(end = 10.dp)
+        )
 
-            FaIcon(
-                faIcon = FaIcons.Bell,
-                size = 30.dp,
-                tint = Color.Gray,
-                modifier = Modifier.padding(end = 10.dp)
-            )
+        FaIcon(
+            faIcon = FaIcons.Bell,
+            size = 30.dp,
+            tint = Color.Gray,
+            modifier = Modifier.padding(end = 10.dp)
+        )
 
-            FaIcon(
-                faIcon = FaIcons.Bars,
-                size = 30.dp,
-                tint = Color.Gray,
-                modifier = Modifier.padding(end = 10.dp)
-            )
+        FaIcon(
+            faIcon = FaIcons.Bars,
+            size = 30.dp,
+            tint = Color.Gray,
+            modifier = Modifier.padding(end = 10.dp)
+        )
+    }
+}
+
+@Composable
+fun CalenderDayNames(modifier: Modifier = Modifier) {
+    val namesList = listOf("일", "월", "화", "수", "목", "금", "토")
+
+    Row(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        namesList.forEach {
+            Box(
+                modifier = Modifier
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = it,
+                    fontSize = 14.sp
+                )
+            }
         }
     }
 }
+
