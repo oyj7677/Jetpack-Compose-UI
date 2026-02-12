d6db2ea4a524f414a75f2527f2cadd3a351335b0
OYJ <oyj@mediazen.co.kr>
2026-02-12 16:12:39 +0900

    35. 달력 앱 만들기 - 3 (달력 UI만들기) + 공식 수정

--- Calender/app/src/main/java/com/example/calender/MainActivity.kt
+++ Calender/app/src/main/java/com/example/calender/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +26,7 @@ import androidx.compose.ui.unit.sp
 import com.example.calender.ui.theme.CalenderTheme
 import com.guru.fontawesomecomposelib.FaIcon
 import com.guru.fontawesomecomposelib.FaIcons
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +43,7 @@ class MainActivity : ComponentActivity() {
                     ) {
                         CalenderHeader()
                         CalenderDayNames()
+                        CalenderDayList()
                     }
                 }
 
@@ -99,3 +105,57 @@ fun CalenderDayNames(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun CalenderDayList(modifier: Modifier = Modifier) {
+    val time = remember { mutableStateOf(Calendar.getInstance()) }
+    val date = time.value
+
+    // 시간 세팅 (2026년 2월)
+    date.set(Calendar.YEAR, 2026)
+    date.set(Calendar.MONTH, Calendar.FEBRUARY) // 0: January, 1: February
+    date.set(Calendar.DAY_OF_MONTH, 1) // 월의 첫날로 설정해야 첫 번째 요일을 정확히 계산
+
+    // 달력 계산 공식 필요값
+    val thisMonthDayMax = date.getActualMaximum(Calendar.DAY_OF_MONTH) // 2월의 마지막 날 (28일)
+    val thisMonthFirstDay = date.get(Calendar.DAY_OF_WEEK) - 1 // 2026년 2월 1일의 요일
+    val thisMonthWeeksCount = (thisMonthDayMax + thisMonthFirstDay + 6) / 7 // 필요한 주 수
+
+    Column(
+        modifier = modifier
+    ) {
+        repeat(thisMonthWeeksCount) { week ->
+            Row() {
+                repeat(7) { day ->
+                    val resultDay = week * 7 + day - thisMonthFirstDay + 1
+
+                    if (resultDay in 1..thisMonthDayMax) {
+                        // 달력 날짜 범위 내
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(60.dp)
+                                .border(1.dp, Color.Gray)
+                        ) {
+                            Text(
+                                text = resultDay.toString(),
+                                fontSize = 14.sp
+                            )
+                        }
+                    } else {
+                        // 범위 밖
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .height(60.dp)
+                                .border(1.dp, Color.Gray)
+                        ) {
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
