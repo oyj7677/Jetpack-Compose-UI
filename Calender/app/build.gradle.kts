dfe8b1cb3103fbff56117aedf5037e4e4cc7f42d
OYJ <oyj@mediazen.co.kr>
2026-02-12 15:01:06 +0900

    33. 달력 앱 만들기 - 1 (fontAwesome)

--- Calender/app/build.gradle.kts
+++ Calender/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -46,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.fontawesomecompose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -53,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
--- Calender/app/src/main/java/com/example/calender/MainActivity.kt
+++ Calender/app/src/main/java/com/example/calender/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calender.ui.theme.CalenderTheme
+import com.guru.fontawesomecomposelib.FaIcon
+import com.guru.fontawesomecomposelib.FaIcons
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +28,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalenderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    CalenderHeader(
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
-            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun CalenderHeader(modifier: Modifier = Modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(top  = 20.dp, bottom = 20.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            FaIcon(
+                faIcon = FaIcons.Cog,
+                size = 30.dp,
+                tint = Color.Gray,
+                modifier = Modifier.padding(end = 10.dp)
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalenderTheme {
-        Greeting("Android")
+            FaIcon(
+                faIcon = FaIcons.Bell,
+                size = 30.dp,
+                tint = Color.Gray,
+                modifier = Modifier.padding(end = 10.dp)
+            )
+
+            FaIcon(
+                faIcon = FaIcons.Bars,
+                size = 30.dp,
+                tint = Color.Gray,
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
     }
 }
